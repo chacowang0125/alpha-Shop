7d0cb8d7a0874d41a5576e3ab580e9f3fa854406
chaco <iloveb9999@gmail.com>
2021-11-01 18:18:46 +0800

    refactor: handleDelivery eventhandler, feat: numberWithCommas function

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ const steps = document.querySelectorAll('.step')
     //form
 const form = document.getElementById('a-form')
 const formParts = form.querySelectorAll('.part')
-const deliveryControl = document.querySelector('.part-content__shipping')
-const freeDelivery = deliveryControl.querySelector('.shipping-selection__original')
-const expressDelivery = deliveryControl.querySelector('.shipping-selection__express')
+const deliveryOptions = document.querySelectorAll('.shipping-selection')
 const deliveryFeeAmount = document.querySelector('.cart-delivery-price')
 const btnControl = document.getElementById('btn-control')
 const prevBtn = btnControl.querySelector('.btn-previous-text')
@@ -22,7 +20,8 @@ let productCost = 5298
 let deliveryFee = 0
 
 //event listener
-deliveryControl.addEventListener('click', handleDelivery)
+//在兩個在兩個運送方式都掛上監聽器
+deliveryOptions.forEach(option => option.addEventListener('click', handleDelivery))
 btnControl.addEventListener('click', handleBtnControl)
 quantityBtns.forEach(btn => btn.addEventListener('click', renderCart))
 darkModeToggle.addEventListener("change", darkModeToggleHandler);
@@ -31,6 +30,10 @@ darkModeToggle.addEventListener("change", darkModeToggleHandler);
 // let numberWithCommas = x => x.toLocaleString();
 //string.toLocalString用Intl代替
 
+function numberWithCommas(num) {
+    return new Intl.NumberFormat('hi-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(num)
+}
+
 //mode switch
 function darkModeToggleHandler(event) {
     event.target.checked ?
@@ -38,22 +41,20 @@ function darkModeToggleHandler(event) {
         document.documentElement.setAttribute("data-theme", "light")
 }
 
+
+
 //select delivery method
 function handleDelivery(e) {
-    console.log(e.target)
-    if (e.target.matches('.shipping-selection__express')) {
-        deliveryFee = 500;
-        deliveryFeeAmount.innerText = '$' + 500
-        freeDelivery.classList.remove('checked')
-        expressDelivery.classList.add('checked')
-    } else if (e.target.matches('.shipping-selection__original')) {
-        deliveryFee = 0;
-        deliveryFeeAmount.innerText = '免費'
-        freeDelivery.classList.add('checked')
-        expressDelivery.classList.remove('checked')
+    deliveryFee = Number(e.currentTarget.querySelector('.shipping-selection__price').innerText.slice(1))
+    deliveryOptions.forEach(option => option.classList.remove('checked'))
+        //在觸發事件時，用currentTarget在綁定事件的節點上增加checked的class
+    e.currentTarget.classList.add('checked')
+    if (!Number(deliveryFee)) {
+        deliveryFee = 0
     }
-    let cartTotal = new Intl.NumberFormat('hi-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(productCost + deliveryFee)
-    cartTotalAmount.innerText = cartTotal;
+
+    deliveryFeeAmount.innerText = numberWithCommas(deliveryFee)
+    cartTotalAmount.innerText = numberWithCommas(productCost + deliveryFee);
 }
 
 //control buttons
@@ -113,6 +114,5 @@ function renderCart(e) {
         }
     }
 
-    let cartTotal = new Intl.NumberFormat('hi-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(productCost + deliveryFee)
-    cartTotalAmount.innerText = cartTotal;
+    cartTotalAmount.innerText = numberWithCommas(productCost + deliveryFee);
 }
